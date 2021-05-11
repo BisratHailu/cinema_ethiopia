032f80ac89b0765921e2c5b35b29b8bff1bcc459
BisratHailu <bisrathailu793@yahoo.com>
2021-05-11 14:51:04 +0300

    First commit

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,10 +1,39 @@
+import 'package:cinema_ethiopia/utilities/scrollPhysics.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../model/color.dart';
 import '../model/lists.dart';
+import '../utilities/scrollPhysics.dart';
 
-class HomePage extends StatelessWidget {
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  final _controller = ScrollController();
+
+  ScrollPhysics _physics;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller.addListener(() {
+      if (_controller.position.haveDimensions && _physics == null) {
+        setState(() {
+          var dimension = _controller.position.maxScrollExtent /
+              (EthioList.popularList.length - 1);
+          _physics = CustomScrollPhysics(itemDimension: dimension);
+        });
+      }
+    });
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -12,48 +41,55 @@ class HomePage extends StatelessWidget {
         backgroundColor: ethioColor.ethioBlack,
         body: CustomScrollView(
           slivers: [
+            SliverAppBar(
+              backgroundColor: ethioColor.ethioBlack,
+              pinned: true,
+              title:   Padding(
+                padding: EdgeInsets.symmetric(
+                    vertical: 10, horizontal: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          "Ethio ",
+                          style: TextStyle(
+                              color: ethioColor.ethioRed, fontSize: 25),
+                        ),
+                        Text(
+                          "Cinema",
+                          style: TextStyle(
+                              color: ethioColor.ethioWhite, fontSize: 25),
+                        ),
+                      ],
+                    ),
+                    IconButton(
+                        icon: Icon(
+                          Icons.search_outlined,
+                          color: ethioColor.ethioWhite,
+                        ),
+                        onPressed: () {})
+                  ],
+                ),
+              ),
+            ),
             SliverToBoxAdapter(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Padding(
-                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          children: [
-                            Text(
-                              "Ethio ",
-                              style: TextStyle(
-                                  color: ethioColor.ethioRed, fontSize: 25),
-                            ),
-                            Text(
-                              "Cinema",
-                              style: TextStyle(
-                                  color: ethioColor.ethioWhite, fontSize: 25),
-                            ),
-                          ],
-                        ),
-                        IconButton(
-                            icon: Icon(
-                              Icons.search_outlined,
-                              color: ethioColor.ethioWhite,
-                            ),
-                            onPressed: () {})
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                    padding: EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 10),
                     child: Text(
                       "Coming Soon",
                       style:
-                          TextStyle(color: ethioColor.ethioWhite, fontSize: 18),
+                      TextStyle(color: ethioColor.ethioWhite, fontWeight: FontWeight.w600),
                     ),
                   ),
                   Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                    padding: EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 10),
                     child: Container(
                       height: 250,
                       width: double.maxFinite,
@@ -68,21 +104,24 @@ class HomePage extends StatelessWidget {
                   ),
                   Container(
                     height: 50,
-                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                    padding: EdgeInsets.symmetric(
+                        vertical: 10, horizontal: 20),
                     child: ListView.builder(
                       scrollDirection: Axis.horizontal,
-                      itemCount: ethioList.movieCategory.length,
+                      itemCount: EthioList.movieCategory.length,
                       itemBuilder: (BuildContext context, int index) {
                         return Container(
                           margin: EdgeInsets.only(right: 15),
-                          padding: EdgeInsets.symmetric(horizontal: 10,vertical: 5),
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 10, vertical: 5),
                           child: Text(
-                            ethioList.movieCategory[index],
+                            EthioList.movieCategory[index],
                             style: TextStyle(
                                 color: ethioColor.ethioWhite, fontSize: 16),
                           ),
                           decoration: BoxDecoration(
-                            borderRadius: BorderRadius.all(Radius.circular(20)),
+                              borderRadius: BorderRadius.all(
+                                  Radius.circular(20)),
                               border: Border.all(
                                   color: ethioColor.ethioRed, width: 0.5)),
                         );
@@ -90,11 +129,33 @@ class HomePage extends StatelessWidget {
                     ),
                   ),
                   Container(
-                    margin: EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+
+                    margin: EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 10),
                     height: 300,
                     child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text("Popular", style: TextStyle(color: ethioColor.ethioWhite,fontSize: 18),)
+                        Padding(
+                          padding:  EdgeInsets.symmetric(vertical: 10),
+                          child: Text("Popular", style: TextStyle(
+                              color: ethioColor.ethioWhite, fontSize: 20,fontWeight: FontWeight.w600),),
+                        ),
+                        Expanded(
+                          child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          controller: _controller,
+                          physics: _physics,
+
+                          itemCount: EthioList.popularList.length,
+                          itemBuilder: (context, index) =>
+                              Container(
+                                height: double.maxFinite,
+                                width: 300,
+
+                                child: Image.asset(EthioList.popularList[index],),
+                              ),
+                        ),),
                       ],
                     ),
                   )
@@ -106,4 +167,5 @@ class HomePage extends StatelessWidget {
       ),
     );
   }
+
 }
--- lib/utilities/scrollPhysics.dart
+++ lib/utilities/scrollPhysics.dart
@@ -2,61 +2,6 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 
-class EthioScroll extends StatefulWidget {
-  const EthioScroll({Key key}) : super(key: key);
-
-  @override
-  _EthioScrollState createState() => _EthioScrollState();
-}
-
-class _EthioScrollState extends State<EthioScroll> {
-
-  final _controller = ScrollController();
-
-  final List<int> pages = List.generate(4, (index) => index);
-
-  ScrollPhysics _physics;
-
-  @override
-  void initState() {
-    super.initState();
-
-    _controller.addListener(() {
-      if (_controller.position.haveDimensions && _physics == null) {
-        setState(() {
-          var dimension =
-              _controller.position.maxScrollExtent / (pages.length - 1);
-          _physics = CustomScrollPhysics(itemDimension: dimension);
-        });
-      }
-    });
-}
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          controller: _controller,
-          physics: _physics,
-          itemCount: pages.length,
-          itemBuilder: (context, index) => Container(
-            height: double.infinity,
-            width: 300,
-            color: randomColor,
-            margin: const EdgeInsets.all(20.0),
-          ),
-        ),
-      ),
-    );
-  }
-
-  Color get randomColor =>
-      Color((Random().nextDouble() * 0xFFFFFF).toInt() << 0).withOpacity(1.0);
-}
-
 class CustomScrollPhysics extends ScrollPhysics {
   final double itemDimension;
 
